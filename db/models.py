dbe17da581146d6a2ca8128c9225297a6571f2db
Oleksandr Dvirko <dvalex02@gmail.com>
2026-05-21 08:41:46 +0300

    passed pytests

--- db/models.py
+++ db/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description =  models.TextField(blank=True)
+    description = models.TextField(blank=True)
 
 
 class Skill(models.Model):
@@ -23,5 +23,5 @@ class Player(models.Model):
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now=True)
--- main.py
+++ main.py
@@ -7,23 +7,33 @@ def main() -> None:
     with open("players.json", "r", encoding="utf-8") as file:
         data = json.load(file)
 
-    for player in data:
-        Player.objects.create(
-            nickname=player,
-            email=player["email"],
-            bio=player["bio"],
-            race=Race.objects.create(
-                name=player["race"]["name"],
-                description=player["race"]["description"]
-            ),
-            guild=Guild.objects.create(
-                name=player["guild"]["name"],
-                description=player["guild"]["description"]
-            ),
-
+    for nickname, player in data.items():
+        race_data = player["race"]
+        race, _ = Race.objects.get_or_create(
+            name=race_data["name"],
+            defaults={"description": race_data["description"]},
         )
 
+        for skill_data in race_data["skills"]:
+            Skill.objects.get_or_create(
+                name=skill_data["name"],
+                defaults={"bonus": skill_data["bonus"], "race": race},
+            )
 
+        guild_data = player["guild"]
+        guild = None
+        if guild_data:
+            guild, _ = Guild.objects.get_or_create(
+                name=guild_data["name"],
+                defaults={"description": guild_data["description"]},
+            )
+        Player.objects.create(
+            nickname=nickname,
+            email=player["email"],
+            bio=player["bio"],
+            race=race,
+            guild=guild,
+        )
 
 
 if __name__ == "__main__":
